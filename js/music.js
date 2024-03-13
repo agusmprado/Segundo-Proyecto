33d39c89a51de24a42bf9fa4300a233c48f66ed3
unknown <pradoagustinadelmilagro9@gmail.com>
2024-03-13 19:53:46 -0300

    se guardo el array de musica reciente en el localStrorage

--- js/music.js
+++ js/music.js
@@ -1,22 +1,27 @@
-( async () => {
-    const idCancion = location.search.split('=')[1]
-    const divMusic = document.getElementById(`music`)
 
-    
-    
-    divMusic.innerHTML = `
-      <div class="d-flex justify-content-center align-items-center">
-         <img src="${datoCancion.img}" width="350" alt="Imagen de la canción">
-      </div>
-       <div class="my-3">
-          <h3>Título de la Canción</h3>
-          <p>${datoCancion.artista}</p>
-          <p>Categoría de la Canción</p>
-      </div>
+const divMusic = document.getElementById(`music`);
+const cancionLocalStorage = JSON.parse(localStorage.getItem(`cancion`)) || [];
+
+const idCancion = location.search.split('=')[1];
+
+const cancionFiltrada = cancionLocalStorage.find((cancion) =>{
+  return cancion.id === Number(idCancion)
+})
+
+if (cancionFiltrada) {
+
+  divMusic.innerHTML = `
+    <div class="d-flex justify-content-center align-items-center">
+      <img src="${cancionFiltrada.img}" width="150" alt="Imagen de la canción" class="rounded-circle img-music">
+    </div>
+    <div class="my-3">
+      <h3 class=" d-flex justify-content-center align-items-center">${cancionFiltrada.nombre}</h3>
+      <p class=" d-flex justify-content-center align-items-center">${cancionFiltrada.artista}</p>
+    </div>
     <audio controls>
-    <source src="" 
-    type="audio/mpeg">  
+      <source src="/pages/error404.html" type="audio/mpeg">  
     </audio>
-    `
+  `;
+} else {
+  divMusic.innerHTML = "<p>No se encontró ninguna canción en el localStorage.</p>";
 }
-)()
